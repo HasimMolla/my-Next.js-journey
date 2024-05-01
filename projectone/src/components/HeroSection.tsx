5ac79c7a6458a7bcdedbfdf5488d021843ba3e6c
Molla1441 <mhasim1441@gmail.com>
2024-05-02 00:03:27 +0530

    add: spotlight form Aceternity UI library

---
+++ projectone/src/components/HeroSection.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { cn } from "@/utils/cn";
+import { Spotlight } from "./ui/spotlight";
+
+
+export const HeroSection = () => {
+  return (
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col justify-center items-center relative overflow-hidden max-auto py-10 md:py-0">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+      <div className="p-4 relative z-10 w-full text-center  ">
+        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+          Master the art of music
+        </h1>
+        <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+          Dive into our comprehensive music courses and transform your musical
+          journey today. Whether you&apos; re a beginner or looking to refine
+          your skills, join us to unlock your true potential.
+        </p>
+        <div className="mt-4">
+          <Link href={"/courses"}>Explore courses</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
--- projectone/tailwind.config.ts
+++ projectone/tailwind.config.ts
@@ -1,4 +1,9 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config: Config = {
   content: [
@@ -9,10 +14,25 @@ const config: Config = {
   darkMode:'class',
   theme: {
     extend: {
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
       },
     },
   },
