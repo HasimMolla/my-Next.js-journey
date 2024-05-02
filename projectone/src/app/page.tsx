9232de43d1c17b2c4b0e10d0693f46b4b0c6a482
Molla1441 <mhasim1441@gmail.com>
2024-05-03 00:42:58 +0530

    add: FEATURED COURSES, grid section , stricky scroll

--- projectone/src/app/page.tsx
+++ projectone/src/app/page.tsx
@@ -1,9 +1,15 @@
+import { FeaturedCourses } from "@/components/FeaturedCourses";
 import { HeroSection } from "@/components/HeroSection";
+import { TestomonialCards } from "@/components/TestomonialCards";
+import { WhyChooseUs } from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] ">
-      <HeroSection/>
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <TestomonialCards/>
     </main>
   );
 }
