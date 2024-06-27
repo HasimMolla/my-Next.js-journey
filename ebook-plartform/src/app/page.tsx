fee748ef1991609701c3ada85c81371d0a987115
Molla1441 <mhasim1441@gmail.com>
2024-06-27 12:56:17 +0530

    setup   nextjs ebook project

---
+++ ebook-plartform/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Welcome to our E-Book PlartForm </h1>
+    </main>
+  );
+}
