4fd3b1909cd5157e8255f90b79713889e3c5c978
Molla1441 <mhasim1441@gmail.com>
2024-06-27 15:15:43 +0530

    add: Footer part

---
+++ ebook-plartform/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="border-t p-5 text-center">
+      <div>
+        © 2024 <span className="text-primary-500 font-semibold">Coder&#39;s Book</span> | All
+        rights reserved.
+      </div>
+    </footer>
+  );
+}
