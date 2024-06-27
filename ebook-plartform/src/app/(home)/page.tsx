a0677aaf0042f12d430221256c544df1fef36340
Molla1441 <mhasim1441@gmail.com>
2024-06-27 15:58:38 +0530

    update: re-organized components

---
+++ ebook-plartform/src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import Banner from "./components/Banner";
+
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-start ">
+      <Banner/>
+    </main>
+  );
+}
