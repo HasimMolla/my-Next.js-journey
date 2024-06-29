4068dd45774996c22d297e9a1dc6292d2e23d16b
Molla1441 <mhasim1441@gmail.com>
2024-06-29 22:41:42 +0530

    add: Book card and Book list

--- ebook-plartform/src/app/(home)/page.tsx
+++ ebook-plartform/src/app/(home)/page.tsx
@@ -1,11 +1,14 @@
 import Banner from "./components/Banner";
+import BookList from "./components/BookList";
 
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start ">
-      <Banner/>
+      <Banner />
+      <BookList/>
+      
     </main>
   );
 }
